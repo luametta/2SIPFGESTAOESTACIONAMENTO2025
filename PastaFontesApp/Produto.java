5f50586730f7d455b85ab7da2446dcd4eb5c6ad2
Luana Metta <160655502+luametta@users.noreply.github.com>
2025-04-09 09:30:52 -0300

    Inclusao de texto explicando a Classe Produto

--- PastaFontesApp/Produto.java
+++ PastaFontesApp/Produto.java
@@ -1,34 +1,32 @@
-
+// Classe que guarda daods de produtos e servicos oferecidos no estacionamento
 public class Produto {
 
 	private double peso;
 	private double altura;
-	
+
 	public Produto() {
-		
+
 	}
-	
+
 	public Produto(double peso, double altura) {
-		this.peso=peso;
-		this.altura=altura;
+		this.peso = peso;
+		this.altura = altura;
 	}
-	
+
 	public double getPeso() {
 		return peso;
 	}
-	
+
 	public void setPeso(double peso) {
 		this.peso = peso;
 	}
-	
+
 	public double getAltura() {
 		return altura;
 	}
-	
+
 	public void setAltura(double altura) {
 		this.altura = altura;
 	}
-	
-	
-	
+
 }
